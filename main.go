19878e15d1cb75863776bb893b47f708834c163d
Ben Cartwright-Cox <Ben@Benjojo.co.uk>
2018-03-28 17:36:17 +0100

    Init checkin

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+type alertManOut struct {
+	Alerts []struct {
+		Annotations struct {
+			Description string `json:"description"`
+			Summary     string `json:"summary"`
+		} `json:"annotations"`
+		EndsAt       string            `json:"endsAt"`
+		GeneratorURL string            `json:"generatorURL"`
+		Labels       map[string]string `json:"labels"`
+		StartsAt     string            `json:"startsAt"`
+		Status       string            `json:"status"`
+	} `json:"alerts"`
+	CommonAnnotations struct {
+		Summary string `json:"summary"`
+	} `json:"commonAnnotations"`
+	CommonLabels struct {
+		Alertname string `json:"alertname"`
+	} `json:"commonLabels"`
+	ExternalURL string `json:"externalURL"`
+	GroupKey    string `json:"groupKey"`
+	GroupLabels struct {
+		Alertname string `json:"alertname"`
+	} `json:"groupLabels"`
+	Receiver string `json:"receiver"`
+	Status   string `json:"status"`
+	Version  string `json:"version"`
+}
+
+type discordOut struct {
+	Content string `json:"content"`
+	Name    string `json:"username"`
+}
+
+func main() {
+	whURL := flag.String("webhook.url", "https://blah", "")
+	flag.Parse()
+	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		amo := alertManOut{}
+		err = json.Unmarshal(b, &amo)
+		if err != nil {
+			panic(err)
+		}
+
+		DO := discordOut{
+			Name: amo.Status,
+		}
+
+		Content := "!!"
+		if amo.CommonAnnotations.Summary != "" {
+			Content = fmt.Sprintf(" === %s === \n", amo.CommonAnnotations.Summary)
+		}
+
+		for _, alert := range amo.Alerts {
+			realname := alert.Labels["instance"]
+			if strings.Contains(realname, "localhost") && alert.Labels["exported_instance"] != "" {
+				realname = alert.Labels["exported_instance"]
+			}
+			Content += fmt.Sprintf("FIRING: %s on %s\n", alert.Labels["alertname"], realname)
+		}
+
+		DO.Content = Content
+
+		DOD, _ := json.Marshal(DO)
+		http.Post(*whURL, "application/json", bytes.NewReader(DOD))
+	}))
+}
